69ff33a12784f88513c6808ebc468f7df55bba2c
juman <jumand909@gmail.com>
2023-07-31 02:33:15 +0530

    error deployment fixed

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const pageNotFound = () => {
+const   PageNotFound = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const pageNotFound = () => {
   )
 }
 
-export default pageNotFound
+export default PageNotFound
